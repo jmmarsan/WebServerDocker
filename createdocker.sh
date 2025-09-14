6b93bf97eabf9c0c33ad4233b5cd37bbbb2d0d4c
José María Martín Sánchez <jmmarsan@gmail.com>
2025-09-14 15:14:34 +0000

    new command

--- createdocker.sh
+++ createdocker.sh
@@ -1,3 +1,4 @@
+sudo docker rm kubia-csharp-container -f
 sudo docker rmi kubia-csharp -f
 sudo docker build -t kubia-csharp .
 sudo docker run --name kubia-csharp-container -p 8080:8080 -d kubia-csharp
