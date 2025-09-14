045917abb1cb36c8b137c206cad2efcd0caa813b
José María Martín Sánchez <jmmarsan@gmail.com>
2025-09-14 21:32:19 +0000

    publish docker script

--- createdocker.sh
+++ createdocker.sh
@@ -1,4 +1,4 @@
-sudo docker rm jmmarsan.webserver-container -f
-sudo docker rmi jmmarsan.webserver -f
-sudo docker build -t jmmarsan.webserver  .
-sudo docker run --name jmmarsan.webserver-container -p 8080:8080 -d jmmarsan.webserver 
+sudo docker rm webserver-container -f
+sudo docker rmi webserver -f
+sudo docker build -t webserver  .
+sudo docker run --name webserver-container -p 8080:8080 -d webserver 
---
+++ publishdocker.sh
@@ -0,0 +1,2 @@
+sudo docker tag webserver jmmarsan/webserver
+sudo docker push jmmarsan/webserver:latest
